00521e5ec7590c9994585f9b12c0b3a0fef0b5eb
Kxxle <64878567+Kxxle@users.noreply.github.com>
2023-11-04 18:13:34 +0100

    I have added the lombok library and I have added getters,setters,constructors,toString and Equals.

--- src/CarWithLombok.java
+++ src/CarWithLombok.java
@@ -1,8 +1,24 @@
+import lombok.*;
+
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@AllArgsConstructor
 public class CarWithLombok {
+
     private String model;
     private String brand;
     private int year;
     private int price;
     private String color;
     private int quantity;
+
+    public void delivery(int amount){
+        this.quantity += amount;
+    }
+    public void sell(int sold){
+        this.quantity -= sold;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-
+        CarWithLombok car1 = new CarWithLombok("GLE","Mercedes",2020,245_980,"white",5);
+        CarWithLombok car2 = new CarWithLombok("GLC","Mercedes",2023,250_000,"green",3);
+        System.out.println(car1.toString());
+        System.out.println(car2.toString());
+        System.out.println(car1.getColor());
+        car1.setColor("black");
+        System.out.println("Are these objects equal? " + car1.equals(car2));
+        System.out.println("The Hashcode of the first object is: "+ car1.hashCode());
+        System.out.println("The Hashcode of the second object is: "+ car2.hashCode());
     }
 }
