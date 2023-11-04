c481923a5c5e6a4c20e5507ca2b0eb55bce4b2b4
Kxxle <64878567+Kxxle@users.noreply.github.com>
2023-11-04 16:31:46 +0100

    Initial commit

---
+++ src/CarWithLombok.java
@@ -0,0 +1,8 @@
+public class CarWithLombok {
+    private String model;
+    private String brand;
+    private int year;
+    private int price;
+    private String color;
+    private int quantity;
+}
